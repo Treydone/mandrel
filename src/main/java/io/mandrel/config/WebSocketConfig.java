6bfad2c0a91dadc31d8c48d4994b021b8757afb2
vdevil <vincent.devillers@catboatmedia.fr>
2015-03-11 18:34:04 +0100

    Begin console output

---
+++ src/main/java/io/mandrel/config/WebSocketConfig.java
@@ -0,0 +1,35 @@
+package io.mandrel.config;
+
+import io.mandrel.common.console.ConsoleWebSocketHandler;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(console(), "/console").addInterceptors(new HttpSessionHandshakeInterceptor()).withSockJS();
+	}
+
+	@Bean
+	public ConsoleWebSocketHandler console() {
+		return new ConsoleWebSocketHandler();
+	}
+
+	@Bean
+	public ServletServerContainerFactoryBean webSocketContainer() {
+		ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+		container.setMaxTextMessageBufferSize(8192);
+		container.setMaxBinaryMessageBufferSize(8192);
+		return container;
+	}
+
+}
