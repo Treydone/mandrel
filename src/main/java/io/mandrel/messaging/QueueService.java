b4789c9651aca229ce891e8f0ca40013fc3b4683
Vincent Devillers <vincent.devillers@layer4.fr>
2015-05-05 11:21:40 +0200

    Maintain a pending url list with ttl

--- src/main/java/io/mandrel/messaging/QueueService.java
+++ src/main/java/io/mandrel/messaging/QueueService.java
@@ -2,6 +2,7 @@ package io.mandrel.messaging;
 
 import java.util.Collection;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
@@ -10,7 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Component;
 
+import com.hazelcast.config.MapConfig;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 import com.hazelcast.core.IQueue;
 
 @Component
@@ -24,17 +27,61 @@ public class QueueService {
 		this.instance = instance;
 	}
 
+	public <T> void markAsPending(String queueName, String identifier, T data) {
+		if (identifier != null) {
+
+			if (instance.getConfig().getQueueConfigs().containsKey(queueName)) {
+				// Create map of pendings with TTL of 2min
+				MapConfig mapConfig = new MapConfig();
+				mapConfig.setName(queueName).setBackupCount(1).setTimeToLiveSeconds(120).setStatisticsEnabled(true);
+				instance.getConfig().addMapConfig(mapConfig);
+			}
+
+			IMap<String, T> pendings = instance.getMap(queueName);
+			pendings.put(identifier, data);
+		}
+	}
+
+	public <T> void removePending(String queueName, String identifier) {
+		if (identifier != null) {
+			IMap<String, T> pendings = instance.getMap(queueName);
+			pendings.remove(identifier);
+		}
+	}
+
+	public <T> Set<T> filterPendings(String queueName, Collection<T> identifiers) {
+		if (identifiers != null) {
+			IMap<String, T> pendings = instance.getMap(queueName);
+			return identifiers.stream().filter(el -> !pendings.containsKey(el)).collect(Collectors.toSet());
+		}
+		return null;
+	}
+
+	public <T> void add(String queueName, T data) {
+		if (data != null) {
+			IQueue<T> queue = instance.getQueue(queueName);
+			queue.offer(data);
+		}
+	}
+
 	public <T> void add(String queueName, Collection<T> data) {
 		if (data != null) {
-			IQueue<Object> queue = instance.getQueue(queueName);
+			IQueue<T> queue = instance.getQueue(queueName);
 			data.forEach(t -> queue.offer(t));
+		}
+	}
+
+	public <T> void remove(String queueName, Collection<T> data) {
+		if (data != null) {
+			IQueue<T> queue = instance.getQueue(queueName);
+			queue.removeAll(data);
 		}
 	}
 
 	public <T> Set<T> deduplicate(String queueName, Collection<T> data) {
 		if (data != null) {
 			IQueue<T> queue = instance.getQueue(queueName);
-			return data.stream().filter(queue::contains).collect(Collectors.toSet());
+			return data.stream().filter(el -> !queue.contains(el)).collect(Collectors.toSet());
 		}
 		return null;
 	}
@@ -47,21 +94,27 @@ public class QueueService {
 	 */
 	public <T> void registrer(String queueName, Callback<T> callback) {
 		boolean loop = true;
-		int nbException = 0;
+		int nbSuccessiveExceptions = 0;
 		// TODO should be parameterized
-		int maxAllowedSuccessiveException = 10;
+		int maxAllowedSuccessiveExceptions = 10;
+
+		int nbSuccessiveWait = 0;
+		// TODO should be parameterized
+		int maxAllowedSuccessiveWait = 10;
 
 		while (loop) {
 			try {
 				// Block until message arrive
-				T message = instance.<T> getQueue(queueName).take();
+				T message = instance.<T> getQueue(queueName).poll(5, TimeUnit.SECONDS);
 				loop = !callback.onMessage(message);
-			} catch (Exception e) {
-				log.warn("Wut?", e);
+				nbSuccessiveExceptions = 0;
+				nbSuccessiveWait = 0;
+			} catch (InterruptedException e) {
+				log.trace("No more message, waiting...");
 
-				nbException++;
-				if (nbException >= maxAllowedSuccessiveException) {
-					log.warn("Too many succesive exceptions, breaking the loop");
+				nbSuccessiveWait++;
+				if (nbSuccessiveWait >= maxAllowedSuccessiveWait) {
+					log.warn("Too many succesive wait, breaking the loop");
 					break;
 				}
 
@@ -69,6 +122,14 @@ public class QueueService {
 					Thread.sleep(5000);
 				} catch (Exception e1) {
 					log.warn("Wut?", e1);
+				}
+			} catch (Exception e) {
+				log.warn("Wut?", e);
+
+				nbSuccessiveExceptions++;
+				if (nbSuccessiveExceptions >= maxAllowedSuccessiveExceptions) {
+					log.warn("Too many succesive exceptions, breaking the loop");
+					break;
 				}
 			}
 		}
