67897797c13ffe69b6639a353a33dfa318b82be3
Vincent Devillers <vincent.devillers@layer4.fr>
2015-05-07 12:10:00 +0200

    Add remove all parameters link filter

---
+++ src/main/java/io/mandrel/data/filters/link/SanitizeParamsFilter.java
@@ -0,0 +1,26 @@
+package io.mandrel.data.filters.link;
+
+import io.mandrel.data.spider.Link;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import org.apache.commons.lang3.StringUtils;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class SanitizeParamsFilter extends LinkFilter {
+
+	private static final long serialVersionUID = -8284466714206360251L;
+
+	public boolean isValid(Link link) {
+		if (link != null && StringUtils.isNotBlank(link.getUri())) {
+			int pos = link.getUri().indexOf('?');
+			if (pos > -1) {
+				link.setUri(link.getUri().substring(0, pos));
+			}
+		}
+		return true;
+	}
+}
---
+++ src/test/java/io/mandrel/data/filters/SanitizeParamsFilterTest.java
@@ -0,0 +1,31 @@
+package io.mandrel.data.filters;
+
+import io.mandrel.data.filters.link.SanitizeParamsFilter;
+import io.mandrel.data.spider.Link;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class SanitizeParamsFilterTest {
+
+	private SanitizeParamsFilter filter = new SanitizeParamsFilter();
+
+	@Test
+	public void no_link() {
+		Assertions.assertThat(filter.isValid(new Link())).isTrue();
+	}
+
+	@Test
+	public void link_without_params() {
+		Link link = new Link().setUri("http://localhost/1");
+		Assertions.assertThat(filter.isValid(link)).isTrue();
+		Assertions.assertThat(link.getUri()).isEqualTo("http://localhost/1");
+	}
+
+	@Test
+	public void link_with_params() {
+		Link link = new Link().setUri("http://localhost/1?foo=test&foo2=test2");
+		Assertions.assertThat(filter.isValid(link)).isTrue();
+		Assertions.assertThat(link.getUri()).isEqualTo("http://localhost/1");
+	}
+}
