0fbabb4275a387de40426330d5d35d02c04c4d93
Vincent Devillers <vincent.devillers@layer4.fr>
2015-07-19 13:30:11 +0200

    Update licensing on sources

--- src/main/java/io/mandrel/endpoints/rest/NodeResource.java
+++ src/main/java/io/mandrel/endpoints/rest/NodeResource.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to Mandrel under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Mandrel licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package io.mandrel.endpoints.rest;
 
 import io.mandrel.cluster.node.Node;
@@ -19,7 +37,7 @@ import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
 @Api("/nodes")
-@RequestMapping(value = "/nodes", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = Apis.PREFIX + "/nodes", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Inject))
 public class NodeResource {
